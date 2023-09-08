e46a6f7c5bccf171e03ce53b9f532f3a70364617
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:28:22 -0600

    Fixed indentation

--- Proyecto/45K50/Corrimiento_bits.c
+++ Proyecto/45K50/Corrimiento_bits.c
@@ -16,16 +16,15 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF                          //Espacio reservado para bootloader
+#pragma orgall 0x1FFF                          // Espacio reservado para bootloader
 
-void main() org 0x2000                         //vector de inicio usando bootloader MICROSIDE
+void main() org 0x2000                         // vector de inicio usando bootloader MICROSIDE
 {
-    TRISB = 0x00;                              //Puerto B como salida
-    PORTB = 0X0D;
+   TRISB = 0x00;                               // Puerto B como salida
+   PORTB = 0X0D;
 
-    while(1){
-        Delay_ms(200);                         //Espera de 200 ms
-        PORTB = (PORTB << 1) | (PORTB >> 7);   //Funcion que realiza el corrimiento en las salidas digitales
-    }
-
+   while ( 1 ) {
+      Delay_ms( 200 );                         // Espera de 200 ms
+      PORTB = ( PORTB << 1 ) | ( PORTB >> 7 ); // Funcion que realiza el corrimiento en las salidas digitales
+   }
 }
